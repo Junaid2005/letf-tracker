cce121bae0e952c919719c88603072702fb6a4fe
Junaid <jm355010@gmail.com>
2025-02-07 16:26:07 +0000

    linted logger

--- main/core/logger.py
+++ main/core/logger.py
@@ -1,30 +1,36 @@
+"""Custom Logger wrapper for the project"""
+
 import logging
+from io import StringIO
 
 
 class Logger:
-    def __init__(self, name="globalLogger"):
+    """Custom logger class"""
+
+    def __init__(self, name="global_logger"):
+        self.log_stream = StringIO()
+        self.stream_handler = logging.StreamHandler(self.log_stream)
+        self.stream_handler.setFormatter(
+            logging.Formatter("%(asctime)s - %(levelname)s: %(message)s")
+        )
+
         logging.basicConfig(
             level=logging.INFO, format="%(asctime)s - %(levelname)s: %(message)s"
         )
         self.logger = logging.getLogger(name)
+        self.logger.addHandler(self.stream_handler)
 
-    def getLevel(self):
+    def get_level(self):
+        """Returns the log level of the logger"""
         return self.logger.getEffectiveLevel()
 
-    def setDebug(self):
-        self.logger.setLevel(logging.DEBUG)
+    def get_logs(self):
+        """Returns all logs"""
+        return self.log_stream.getvalue()
 
-    def log(self, level, message):
-        if level == "debug":
-            self.logger.debug(message)
-        elif level == "info":
-            self.logger.info(message)
-        elif level == "warning":
-            self.logger.warning(message)
-        elif level == "error":
-            self.logger.error(message)
-        elif level == "critical":
-            self.logger.critical(message)
+    def set_debug(self):
+        """Sets the logger to debug mode"""
+        self.logger.setLevel(logging.DEBUG)
 
 
 logger = Logger()
