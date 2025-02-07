2d06f92e6dcb879fd88e1c77e64f77ca7f739ba4
Junaid <jm355010@gmail.com>
2025-02-07 16:31:07 +0000

    refactored and linted

--- main/models/security_pair.py
+++ main/models/security_pair.py
@@ -1,47 +1,141 @@
-import yfinance as yf
+"""File defines the SecurityPair class that manages LETF and Underlying Security Relationship"""
+
+# pylint: disable=broad-exception-caught
+# this is just laziness tbh i should find the errors ^
 from datetime import datetime, timedelta
-import pandas_market_calendars as mcal
-import re
-import pytz
 import sys
 import os
+import copy
 import pandas as pd
+import pandas_market_calendars as mcal
+import pytz
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+# pylint: disable=wrong-import-position
 from core.logger import logger
+from models.cache_manager import cache_manager
+from models.yfinance_wrapper import YFinanceSecurity
+
+# update all 'yf_ticker' to represent what the ticker is, letf or the underlying WHERE IT MAKES
 
 
-class securityPair:
+class SecurityPair:
+    """Class holding the leveraged etf and its underlying security"""
+
     def __init__(self, underlying, letf):
         self.underlying = underlying
         self.letf = letf
 
-        # add way to make sure tickers are real
-        self.underlyingTicker = yf.Ticker(underlying)
-        self.letfTicker = yf.Ticker(letf)
+        self.underlying_ticker = YFinanceSecurity(underlying)
+        self.letf_ticker = YFinanceSecurity(letf)
 
-    def main(self):
-        close_time = self.get_closing_time(self.letfTicker)
+    def is_valid_pair(self) -> bool:
+        """Returns if both of the tickers provided are found by yfinance"""
+        return (
+            self.underlying_ticker.is_real_security()
+            and self.letf_ticker.is_real_security()
+        )
+
+    def get_percent_return(self):
+        """Returns extended hours percentage returns in a pair (or error)"""
+        # is this valid behaviour
+        # should i really be telling the user to stop using the tool if both pairs are live
+        if self.is_pair_live():
+            return "Pair is already in sync"
+
+        close_time = self.get_closing_time(self.letf_ticker)
         if not isinstance(close_time, pd.Timestamp):
             return close_time
 
-        leverage = self.get_leverage(self.letfTicker)
+        target_timezone = self.underlying_ticker.get_timezone()
+        open_time = close_time.astimezone(target_timezone)
+
+        change = self.underlying_ticker.get_pricing_change(
+            open_time, datetime.now(target_timezone)
+        )
+        if isinstance(change, str):
+            return change
+
+        leverage = self.letf_ticker.get_leverage()
         if leverage == "Error finding leverage":
             return leverage
 
-        target_timezone = self.get_timezone(self.underlyingTicker)
-        open_time = close_time.astimezone(target_timezone)
-
-        change = self.get_pricing_change(open_time, datetime.now(target_timezone))
-        if isinstance(change, str):
-            return change
         return self.calculate_return(leverage, change)
 
-    def get_closing_time(self, yf_ticker):
-        info = yf_ticker.info
-        exchange_str = info.get(
-            "exchange", f"Error: Can't find exchange for {yf_ticker}"
+    def get_absolute_return(self, ccy_rates, percent_return):
+        """Returns absolute return in a pair (or error)"""
+        try:
+            change = float(percent_return.rstrip("%")) / 100
+        except ValueError:
+            return "No return to calculate real value from"
+
+        base_ccy = cache_manager.get("CURRENCY_CODE")
+        security = copy.deepcopy(cache_manager.get_security(self.letf))
+        if security.get("error"):
+            return security["error"]
+
+        raw_value = security["quantity"] * security["averagePrice"]
+        logger.logger.debug(
+            f"Multiplying {security['quantity']} by {security['averagePrice']} to get {raw_value}",
         )
+
+        if security["currencyCode"] == "GBX":
+            raw_value /= 100
+            security["currencyCode"] = "GBP"
+            logger.debug(f"Converted GBX to GBP. New value is {raw_value}")
+
+        if security["currencyCode"] != base_ccy:
+            raw_value = ccy_rates.convert(raw_value, security["currencyCode"], base_ccy)
+            logger.logger.debug(
+                f"Converted {security['currencyCode']} to {base_ccy}. New value is {raw_value}",
+            )
+
+        ppl = security.get("ppl")
+        fx_ppl = security.get("fxPpl") or 0
+        original_value = raw_value + ppl + fx_ppl
+        logger.logger.debug(
+            f"Added PPLs (ppl: {ppl}, fxPpl: {fx_ppl}) to get {original_value}",
+        )
+        live_value = original_value * change
+        logger.logger.debug(
+            f"Multiplied {original_value} by {change} to get {live_value}"
+        )
+        return live_value
+
+    def __is_security_live(self, yf_ticker):
+        name = yf_ticker.security.info.get("shortName", "")
+        cal = self.__get_exchange_cal(yf_ticker)
+        if isinstance(cal, str):
+            logger.log("error", cal)
+            return False
+        schedule = self.__get_schedule(cal)
+        if isinstance(schedule, str):
+            logger.log("error", f"{schedule} for {name}")
+            return False
+
+        now_utc = datetime.now(pytz.utc)
+        now_timestamp = pd.Timestamp(now_utc)
+        converted_timestamp = now_timestamp.tz_convert(
+            yf_ticker.get_timezone(log=False)
+        )
+        try:
+            is_open = cal.open_at_time(schedule, converted_timestamp)
+        except (ValueError, IndexError):
+            is_open = False
+        logger.debug(f"{name} is currently {'trading' if is_open else 'closed'}")
+        return is_open
+
+    def is_pair_live(self) -> bool:
+        """Returns if both of the securities are currently trading"""
+        return self.__is_security_live(self.letf_ticker) and self.__is_security_live(
+            self.underlying_ticker
+        )
+
+    def get_closing_time(self, yf_ticker_letf):
+        """Returns last closing time of letf"""
+        info = yf_ticker_letf.security.info
+        name = info.get("symbol", "Couldn't find symbol")
+        exchange_str = info.get("exchange", f"Error: Can't find exchange for {name}")
         exchange = mcal.get_calendar(exchange_str)
 
         schedule = exchange.schedule(
@@ -52,70 +146,53 @@ class securityPair:
         recent_closes.reverse()
 
         for close in recent_closes:
-            if close < pytz.UTC.localize(datetime.now()):
-                logger.log("debug", f"{yf_ticker} closed at {close}")
+            if close < datetime.now(pytz.utc):
+                logger.debug(f"{name} closed at {close}")
                 return close
 
         return "Error: Can't find a recent close"
 
-    def get_leverage(self, yf_ticker):
-        info = yf_ticker.info
-        # ADD SOMETHING FOR THE WORD 'SHORT/INVERSE'
-        error_message = "Error finding leverage"
-        longName = info.get("longName", error_message)
-        match = re.search(r"(-?\d+x)", longName)
-        if match:
-            leverage = match.group(0)
-            logger.log("debug", f"Found leverage for {yf_ticker} to be {leverage}")
-            return leverage
-
-        logger.log("debug", error_message)
-        return error_message
-
-    def get_timezone(self, yf_ticker):
-        info = yf_ticker.info
-        timezone_name = info.get("timeZoneFullName", "Not found")
-        logger.log("debug", f"{yf_ticker} found to be in timezone {timezone_name}")
-        return pytz.timezone(timezone_name)
-
-    def get_pricing_change(self, start_timestamp, current_timestamp):
-        open_start_range = start_timestamp - timedelta(minutes=5)
-        open_end_range = start_timestamp + timedelta(minutes=5)
-
-        logger.log("debug", f"Current datetime {current_timestamp}")
-
-        data = self.underlyingTicker.history(
-            start=open_start_range, end=open_end_range, interval="1m"
-        )
-        if data.empty:
-            logger.log(
-                "debug", f"Unable to price {self.underlying} at {start_timestamp}"
-            )
-            return "Error getting pricing"
-        elif start_timestamp in data.index:
-            start_price = data.loc[start_timestamp].Open
-            logger.log(
-                "debug",
-                f"Price of {self.underlying} at {start_timestamp}: {start_price}",
-            )
-        else:
-            start_price = data.iloc[0].Open
-            logger.log(
-                "debug",
-                f"Failed to price {self.underlying} at open. Using {data.iloc[0].name}: {start_price}",
-            )
-
-        data = self.underlyingTicker.history(period="1d", interval="1m", prepost=True)
-        current_price = data["Close"].iloc[-1]
-        logger.log(
-            "debug",
-            f"Current price of {self.underlying} at {current_timestamp}: {current_price}",
-        )
-        percent = (current_price - start_price) * 100 / start_price
-        return percent
-
     def calculate_return(self, leverage, change):
+        """Multiplies percentage return by leverage found"""
         leverage = int(leverage[:-1])
         result = leverage * change
-        logger.log("debug", f"Multiplied {leverage} by {change} resulting in {result}")
+        logger.debug(f"Multiplied {leverage} by {change} resulting in {result}")
         return f"{round(result, 2)}%"
+
+    def __get_exchange_cal(self, yf_ticker):
+        yf_ticker_info = yf_ticker.security.info
+        exchange_str = yf_ticker_info.get(
+            "exchange", f"Error: Can't find exchange for {yf_ticker}"
+        )
+        exchange_map = {"NMS": "NASDAQ", "NGM": "NASDAQ"}
+        try:
+            exchange_cal = mcal.get_calendar(
+                exchange_map.get(exchange_str, exchange_str)
+            )
+            return exchange_cal
+        except Exception:
+            name = yf_ticker_info.get("shortName", "")
+            error_message = f"Could not get calendar for exchange {name}"
+            return f"Error: {error_message}"
+
+    def __get_schedule(
+        self,
+        exchange_cal,
+        start=datetime.now().today(),
+        end=datetime.now().today(),
+    ):
+        try:
+            schedule = exchange_cal.schedule(
+                start_date=start, end_date=end, start="pre", end="post"
+            )
+            return schedule
+        except Exception:
+            logger.log("warn", "Exchange has no extended hours")
+            try:
+                schedule = exchange_cal.schedule(
+                    start_date=start,
+                    end_date=end,
+                )
+                return schedule
+            except Exception:
+                return "Error getting schedule for exchange"
