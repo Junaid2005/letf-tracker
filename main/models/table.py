738f565daadb5246a4ea6e0bf70a3b8a55f432df
Junaid <jm355010@gmail.com>
2025-02-07 16:33:30 +0000

    linted

--- main/models/table.py
+++ main/models/table.py
@@ -1,41 +1,64 @@
+"""File that handles creating table for cli or email"""
+
+from datetime import datetime
 from colorama import Fore, Style
 from tabulate import tabulate
 import pandas as pd
-from datetime import datetime
 
 
-class tabulateTable:
+class TabulateTable:
+    """Class that prepares table whether thats for cli or email"""
+
     def __init__(self, data):
         self.data = data
         self.df = pd.DataFrame(data)
 
     def reformat_data(self, data):
+        """Apply colour formatting for the numerical columns"""
         return [
-            [row["Underlying"], row["LETF"], self.color_change(row["Change"])]
+            [
+                (
+                    self.color_change(row[column])
+                    if column in ["Ext Hours %", "Ext Hours"]
+                    else row[column]
+                )
+                for column in row
+            ]
             for row in data
         ]
 
     def color_change(self, value):
-        if not "%" in value:
-            return value
-        num = float(value.replace("%", ""))
-        if num > 0:
-            return Fore.GREEN + value + Style.RESET_ALL
-        elif num < 0:
+        """Method to change colour within the df itself based on numeric value"""
+        value = str(value)
+        try:
+            num = float(value.rstrip("%"))
+            if num > 0:
+                return Fore.GREEN + value + Style.RESET_ALL
+            elif num < 0:
+                return Fore.RED + value + Style.RESET_ALL
+
+        except ValueError:
             return Fore.RED + value + Style.RESET_ALL
 
     def print_table(self):
-        tabulateData = self.reformat_data(self.data)
+        """Prints out the table, used for cli"""
+        tabulate_data = self.reformat_data(self.data)
+        align = (
+            ("left", "left", "right")
+            if len(tabulate_data[0]) == 3
+            else ("left", "left", "right", "right")
+        )
         print(
             tabulate(
-                tabulateData,
+                tabulate_data,
                 headers=self.df.columns,
                 tablefmt="fancy_grid",
-                colalign=("left", "left", "right"),
+                colalign=align,
             )
         )
 
     def prepare_html_table(self):
+        """Converts the table for html, used for email"""
         current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         html_table = self.df.to_html(
             index=False, border=0, escape=False, classes="email-table"
